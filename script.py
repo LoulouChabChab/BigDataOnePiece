4d55aa074edfc788cecdc1080e843f041ba4d845
LoulouChabChab <louis.chabert99@orange.fr>
2022-03-22 16:53:09 +0100

    add script image color

--- script.py
+++ script.py
@@ -1,17 +1,34 @@
 from PIL import Image
-from PIL.ExifTags import TAGS
+import numpy
+import math
+import matplotlib.pyplot as plot
+from sklearn.cluster import KMeans
 
-image = Image.open("Data/Ace/20.jpg")
+image = Image.open("Data/Ace/75.png")
 # extract other basic metadata
 info_dict = {
     "Filename": image.filename,
     "Image Height": image.height,
-    "Image Width": image.width,
+    "Image Width": image.width
 }
 
 for label, value in info_dict.items():
     print(f"{label:25}: {value}")
 
+
+numarray = numpy.array(image.getdata(), numpy.uint8)
+clusters = KMeans(n_clusters=4)
+clusters.fit(numarray)
+npbins = numpy.arange(0, 5)
+histogram = numpy.histogram(clusters.labels_, bins=npbins)
+labels = numpy.unique(clusters.labels_)
+barlist = plot.bar(labels, histogram[0])
+for i in range(4):
+    barlist[i].set_color('#%02x%02x%02x' % (
+        math.ceil(clusters.cluster_centers_[i][0]),
+        math.ceil(clusters.cluster_centers_[i][1]),
+        math.ceil(clusters.cluster_centers_[i][2])))
+plot.show()
 # # extract EXIF data
 # exifdata = image.getexif()
 #

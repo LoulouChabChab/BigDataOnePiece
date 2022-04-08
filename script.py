e9cc1fe021c3df9e323e7ffdb5bdac970eefe516
axel <axel.de-freitas@cpe.fr>
2022-04-08 16:47:26 +0200

    Push étrange pour louis

--- script.py
+++ script.py
@@ -1,38 +1,62 @@
-import pandas as pd
-from PIL import Image
-import numpy
 import math
+# import required module
+import os
+
 import matplotlib.pyplot as plot
+import numpy
+from PIL import Image
 from sklearn.cluster import KMeans
 
-image = Image.open("Data/Ace/75.png")
-# extract other basic metadata
-info_dict = {
-    "Filename": image.filename,
-    "Image Height": image.height,
-    "Image Width": image.width
-}
-
-for label, value in info_dict.items():
-    print(f"{label:25}: {value}")
+# assign directory
+directory = 'Data/Robin'
+index = 0
 
 
-numarray = numpy.array(image.getdata(), numpy.uint8)
-clusters = KMeans(n_clusters=3)
-clusters.fit(numarray)
-npbins = numpy.arange(0, 4)
-histogram = numpy.histogram(clusters.labels_, bins=npbins)
-labels = numpy.unique(clusters.labels_)
-barlist = plot.bar(labels, histogram[0])
-for i in range(3):
-    barlist[i].set_color('#%02x%02x%02x' % (
-        math.ceil(clusters.cluster_centers_[i][0]),
-        math.ceil(clusters.cluster_centers_[i][1]),
-        math.ceil(clusters.cluster_centers_[i][2])))
-plot.show()
+# iterate over files in
+# that directory
+def data(file):
+    image = Image.open(file)
+    # extract other basic metadata
+    info_dict = {
+        "Filename": image.filename,
+        "Image Height": image.height,
+        "Image Width": image.width
+    }
 
-df = pd.DataFrame()
-df.to_csv('mystake.csv', mode='a', header=False)
+    for label, value in info_dict.items():
+        print(f"{label:25}: {value}")
+
+    nbColors = 2
+    numarray = numpy.array(image.getdata(), numpy.uint8)
+    clusters = KMeans(n_clusters=nbColors)
+    if numarray.ndim == 1:
+        return
+    clusters.fit(numarray)
+    npbins = numpy.arange(0, nbColors + 1)
+    print(npbins)
+    histogram = numpy.histogram(clusters.labels_, bins=npbins)
+    labels = numpy.unique(clusters.labels_)
+    barlist = plot.bar(labels, histogram[0])
+    for i in range(nbColors):
+        print("Couleur ", i, " : ")
+        print('#%02x%02x%02x' % (math.ceil(clusters.cluster_centers_[i][0]), math.ceil(clusters.cluster_centers_[i][1]),
+                                 math.ceil(clusters.cluster_centers_[i][2])))
+        barlist[i].set_color('#%02x%02x%02x' % (
+            math.ceil(clusters.cluster_centers_[i][0]),
+            math.ceil(clusters.cluster_centers_[i][1]),
+            math.ceil(clusters.cluster_centers_[i][2])))
+    plot.show()
+
+
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    # checking if it is a file
+    if os.path.isfile(f):
+        data(f)
+
+# df = pd.DataFrame()
+# df.to_csv('mystake.csv', mode='a', header=False)
+
 # # extract EXIF data
 # exifdata = image.getexif()
 #
